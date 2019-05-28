87cd2e0ab2a3ac0a816b44893bf6776a15055a2e
Todd Elvers <todd.elvers@gmail.com>
2019-05-27 19:01:27 -0500

    Removed the final TODO

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,17 +35,3 @@ tasks {
         testLogging.exceptionFormat = TestExceptionFormat.FULL
     }
 }
-
-
-/*
-        TODO: Decide if we want to include spock-reports
-        // Dependencies
-        testRuntime("org.slf4j:slf4j-simple:1.7.26")
-        testRuntime("com.athaydes:spock-reports:1.6.2")
-
-        // simpleLogger.properties
-        org.slf4j.simpleLogger.defaultLogLevel=ERROR
-        org.slf4j.simpleLogger.logFile=System.out
-        org.slf4j.simpleLogger.showThreadName=false
-        org.slf4j.simpleLogger.levelInBrackets=true
- */
